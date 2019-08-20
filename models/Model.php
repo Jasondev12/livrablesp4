94b026c0d52dd32c30605e8ee7d1c83068445317
Jason120791 <maesjasonpro@gmail.com>
2019-08-20 10:08:29 +0200

    MAJ 20/08 MVC

---
+++ models/Model.php
@@ -0,0 +1,41 @@
+<?php
+
+abstract class Model
+{
+   private static $_bdd;
+   private static function setBdd()
+   {
+     private $dbhost = 'localhost';
+     private $dbname = 'blogp4';
+     private $dbuser = 'root';
+     private $dbpswd = '';
+
+     self::$_bdd = new PDO('mysql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpswd);
+     self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO:ERRMODE_WARNING)
+   }
+   protected function getBdd(){
+    if(self::$_bdd == null)
+    $this->setBdd();
+    return self::$_bdd;
+   }
+   protected function getAll($table, $obj){
+     $var= [];
+     $req = self::$_bdd->prepare( 'SELECT * FROM . table . ORDER BY id desc');
+     $req->execute();
+     while ($data = $req->fetch(PDO::FETCH_ASSOC)){
+       $var[] = new $obj($data);
+     }
+     return $var;
+     $req->closeCursor();
+   }
+
+    console.log('', );
+
+}
+
+
+
+
+
+
+ ?>
